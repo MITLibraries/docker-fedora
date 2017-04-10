f68daed5c31c149ebbd1123cdff51a695a717ef8
Mike Graves <mgraves@mit.edu>
2017-04-10 10:50:17 -0400

    Increase fedora memory to 2GB

--- setenv.sh
+++ setenv.sh
@@ -1,4 +1,5 @@
 JAVA_OPTS="$JAVA_OPTS \
+  -Xmx2048M
   -Dfcrepo.modeshape.configuration=classpath:/config/file-simple/repository.json \
   -Dfcrepo.activemq.configuration=file:/etc/fcrepo/activemq.xml \
   -Dfcrepo.home=/fcrepo4-data"
